e8ddbcee8c9d4b36761dcb92d2f28bb65a4cb0e8
muhammadekafirdaus <49117901+muhammadekafirdaus@users.noreply.github.com>
2019-03-31 18:51:19 +0700

    Add files via upload

---
+++ MuhammadEkaFirdaus.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ cout << "Nama : Muhammad Eka Firdaus"<<endl;
+ cout << "NIM  : 1829142009"<<endl;
+cout << "Kelas: Tekom B"<<endl;
+cout << "Tahun: 2018"<<endl;
+
+}
